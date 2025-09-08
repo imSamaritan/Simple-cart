db863f0b7d34f49306633883625096ef388fafaa
imsamaritan <aicodemate.talk@gmail.com>
2025-09-08 20:45:17 +0200

    Calculate the total cart items inside the cartContxt using createMemo as computed derived state and passed it down to the Header component and recreated nav links through createMemo

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,13 +1,16 @@
-import { createContext, useContext } from "solid-js"
+import { createContext, useContext, createMemo } from "solid-js"
 import { createStore } from "solid-js/store"
 
 const CartContext = createContext()
 
 function CartContextProvider(props) {
   const [items, setItem] = createStore([])
+  let total = createMemo(() => {
+    return items.reduce((acc, item) => acc + item.quantity, 0)
+  })
 
   return (
-    <CartContext.Provider value={{ items, setItem }}>
+    <CartContext.Provider value={{ items, setItem, total }}>
       {props.children}
     </CartContext.Provider>
   )

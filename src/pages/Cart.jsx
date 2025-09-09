fcc780733aba9b4671574c5a88e6e6cd878aa960
imsamaritan <aicodemate.talk@gmail.com>
2025-09-09 22:49:57 +0200

    Created decrement and increment buttons for each added cart item/product and imported addToCart method inside the cart component for increment button to trigger for incrementing the added item/product if needed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,10 @@
 import { usingCartContext } from "../context/CartContext"
 import { For } from "solid-js"
 import Card from "../components/Card"
+import Button from "../components/Button"
 
 function Cart() {
-  const { items } = usingCartContext()
+  const { items, addToCart } = usingCartContext()
   const formartZAR = new Intl.NumberFormat("en-SA", {
     style: "currency",
     currency: "ZAR",
@@ -13,9 +14,9 @@ function Cart() {
       <div class="card-content">
         <For each={items}>
           {(item) => (
-            <div class="columns is-gapless">
-              <div class="column is-1">
-                <img width="80" class="avatar" src={item.img} alt="" />
+            <div class="columns is-gapless has-text-centered-mobile">
+              <div class="column is-2">
+                <img width="130" class="avatar" src={item.img} alt="" />
               </div>
               <div class="column is-2">
                 <p>{item.title}</p>
@@ -23,6 +24,14 @@ function Cart() {
                 <p>
                   Price:
                   {formartZAR.format(item.price * 2 * 1.9 * item.quantity)}
+                </p>
+                <p class="mt-2">
+                  <Button class="decrease button has-text-light has-background-danger has-text-weight-bold">
+                    -
+                  </Button>
+                  <Button class="increase button has-text-light has-background-success has-text-weight-bold" onClick={() => addToCart(item.id, item)}>
+                    +
+                  </Button>
                 </p>
               </div>
             </div>

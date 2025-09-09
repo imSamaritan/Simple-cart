2fbfe64efc9dff4e06f1d0e8e9c183c8acda60b2
imsamaritan <aicodemate.talk@gmail.com>
2025-09-09 22:11:47 +0200

    moved addToCart method to CartContext component, which takes two params (id, productDetails). Product component is now responsible for rendering single product and triggering adding a product to cart/ buy

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,12 +5,34 @@ const CartContext = createContext()
 
 function CartContextProvider(props) {
   const [items, setItem] = createStore([])
+  
   let total = createMemo(() => {
     return items.reduce((acc, item) => acc + item.quantity, 0)
   })
 
+  const addToCart = (id, product) => {
+    let item = items.find((product) => product.id === id)
+
+    if (item === undefined) {
+      item = {
+        id: product.id,
+        title: product.title,
+        quantity: 1,
+        price: product.price,
+        img: product.img,
+      }
+      setItem([item, ...items])
+    } else {
+      setItem(
+        (product) => product.id === id,
+        "quantity",
+        (q) => (q += 1)
+      )
+    }
+  }
+
   return (
-    <CartContext.Provider value={{ items, setItem, total }}>
+    <CartContext.Provider value={{ items, setItem, total, addToCart }}>
       {props.children}
     </CartContext.Provider>
   )
